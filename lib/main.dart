7360abd51539ee77960b0090c6daf4bc9a6b2e45
Mileba <mykellforbes@gmail.com>
2020-06-17 15:19:27 +0100

    app challenge

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,56 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+            child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                Container(
+                  color: Colors.red,
+                  width: 100,
+                  height: 650,
+                )
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  color: Colors.yellow,
+                  width: 100,
+                  height: 100,
+                ),
+                Container(
+                  color: Colors.greenAccent,
+                  width: 100,
+                  height: 100,
+                ),
+              ],
+            ),
+            Column(
+              children: <Widget>[
+                Container(
+                  color: Colors.blue,
+                  width: 100,
+                  height: 650,
+                )
+              ],
+            )
+          ],
+        )),
       ),
-    ),
-  );
+    );
+  }
 }
